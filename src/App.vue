e753e5b304b946606b1df1cc6c2756dff3fdef75
Jiri George Holy <george@h01y.com>
2019-08-26 11:44:52 +1200

    add open games into navigation menu

--- src/App.vue
+++ src/App.vue
@@ -14,26 +14,57 @@
 
       <v-divider></v-divider>
 
-      <v-list
-      >
-        <v-list-item
-          v-for="item in menuLinks"
-          :key="item.title"
-          link
-          :to="item.link"
+      <v-list>
+        <template
+          v-for="menuLink in menuLinks"
         >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+          <v-list-group
+            v-if="menuLink.children && menuLink.children.length"
+            :prepend-icon="menuLink.icon"
+            :key="menuLink.link"
+          >
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>{{ menuLink.title }}</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item
+              link
+              v-for="battle in menuLink.children"
+              :to="battle.link"
+              :key="battle.link"
+            >
+              <v-list-item-icon>
+                <v-icon>{{ battle.icon }}</v-icon>
+              </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
+              <v-list-item-content>
+                <v-list-item-title><span v-html="battle.title"/></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list-group>
+          <template
+            v-else
+          >
+            <v-list-item
+              link
+              :to="menuLink.link"
+              :key="menuLink.link"
+            >
+              <v-list-item-icon>
+                <v-icon>{{ menuLink.icon }}</v-icon>
+              </v-list-item-icon>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ menuLink.title }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </template>
       </v-list>
       <template v-slot:append v-if="isAuthenticated">
         <div class="pa-2">
-          <v-btn block @click="this.$store.dispatch('logout')">Logout</v-btn>
+          <v-btn block @click="$store.dispatch('logout')">Logout</v-btn>
         </div>
       </template>
     </v-navigation-drawer>
@@ -60,14 +91,24 @@ export default {
     defaultLinks: function () {
       return [
         { title: 'Home', icon: 'mdi-home', link: '/' },
-        { title: 'Login', icon: 'mdi-key', link: '/login', auth: false },
-        { title: 'Neptune', icon: 'mdi-image', link: '/neptune', auth: true },
-        { title: this.currentUser.user_email, icon: 'account_box', link: '/profile', auth: true }
+        { title: this.currentUser.user_email, icon: 'account_box', link: '/profile', auth: true },
+        { title: 'Login', icon: 'mdi-key', link: '/login', auth: false }
       ]
     },
     menuLinks: function () {
-      var links = this.defaultLinks.filter(p => typeof p.auth === 'undefined' || (p.auth === this.isAuthenticated))
-      return links
+      var links = this.defaultLinks
+
+      if (typeof this.currentUser.open_games !== 'undefined') {
+        var gameLinks = this.currentUser.open_games.map(function (g) { return {title: unescape(g.name), icon: 'mdi-battlenet', link: `/game/${g.number}`} })
+        links.push({ title: 'Neptune',
+          icon: 'mdi-image',
+          link: '/neptune',
+          auth: true,
+          children: gameLinks
+        })
+      }
+
+      return links.filter(p => typeof p.auth === 'undefined' || (p.auth === this.isAuthenticated))
     }
   },
   mounted: function () {
