2f5646b160d81a2988d03d4672887377843e11cb
Jiri George Holy <george@h01y.com>
2019-08-29 20:47:18 +0000

    colorize players table

--- src/components/game/Players.vue
+++ src/components/game/Players.vue
@@ -7,21 +7,21 @@
     hide-default-footer
     dense
   >
-    <template v-slot:item.total_stars="{ item }">
-      <v-chip :color="getColorShare(item, 'total_stars')" >{{ item.total_stars }}</v-chip>
+
+      <template v-slot:body="{ items }">
+      <tbody>
+        <tr v-for="item in items" :key="item.name">
+          <template v-for="property in headers">
+            <td :key="property.value" v-bind:style="getColorStyle(item, property)">{{ resolve(property.value, item) }}</td>
+          </template>
+        </tr>
+      </tbody>
     </template>
-    <template v-slot:item.tech.weapons.level="{ item }">
-      <v-chip :color="getColorShare(item, 'tech.weapons.level')" >{{ item.tech.weapons.level }}</v-chip>
-    </template>
+
   </v-data-table>
 </template>
 
 <script>
-
-const resolve = (path, obj, separator = '.') => {
-  var properties = Array.isArray(path) ? path : path.split(separator)
-  return properties.reduce((prev, curr) => prev && prev[curr], obj)
-}
 
 export default {
   props: ['value'],
@@ -33,27 +33,38 @@ export default {
           align: 'left',
           value: 'alias'
         },
-        { text: 'Ships production', value: 'ships_production' },
-        { text: 'Money production', value: 'money_production' },
-        { text: 'Stars', value: 'total_stars' },
-        { text: 'Ships', value: 'total_strength' },
-        { text: 'Industry', value: 'total_industry' },
-        { text: 'Economy', value: 'total_economy' },
-        { text: 'Science', value: 'total_science' },
-        { text: 'Scanning', value: 'tech.scanning.level' },
-        { text: 'Propulsion', value: 'tech.propulsion.level' },
-        { text: 'Terraforming', value: 'tech.terraforming.level' },
-        { text: 'Research', value: 'tech.research.level' },
-        { text: 'Weapons', value: 'tech.weapons.level' },
-        { text: 'Banking', value: 'tech.banking.level' },
-        { text: 'Manufacturing', value: 'tech.manufacturing.level' }
+        { text: 'Ships production', value: 'ships_production', colorized: true },
+        { text: 'Money production', value: 'money_production', colorized: true },
+        { text: 'Stars', value: 'total_stars', colorized: true },
+        { text: 'Ships', value: 'total_strength', colorized: true },
+        { text: 'Industry', value: 'total_industry', colorized: true },
+        { text: 'Economy', value: 'total_economy', colorized: true },
+        { text: 'Science', value: 'total_science', colorized: true },
+        { text: 'Scanning', value: 'tech.scanning.level', colorized: true },
+        { text: 'Propulsion', value: 'tech.propulsion.level', colorized: true },
+        { text: 'Terraforming', value: 'tech.terraforming.level', colorized: true },
+        { text: 'Research', value: 'tech.research.level', colorized: true },
+        { text: 'Weapons', value: 'tech.weapons.level', colorized: true },
+        { text: 'Banking', value: 'tech.banking.level', colorized: true },
+        { text: 'Manufacturing', value: 'tech.manufacturing.level', colorized: true }
       ]
     }
   },
   methods: {
-    getColorShare (player, property, reverse = false) {
-      const playerValue = resolve(property, player)
-      const sorted = Object.values(this.value.players).map(p => resolve(property, p)).sort((a, b) => (a > b) ? 1 : -1).filter((value, index, self) => self.indexOf(value) === index)
+    resolve (path, obj, separator = '.') {
+      var properties = Array.isArray(path) ? path : path.split(separator)
+      return properties.reduce((prev, curr) => prev && prev[curr], obj)
+    },
+    getColorStyle (player, property) {
+      const res = {}
+      if (property.colorized) {
+        res['background-color'] = this.getColorShare(player, property.value)
+      }
+      return res
+    },
+    getColorShare (player, propertyName, reverse = false) {
+      const playerValue = this.resolve(propertyName, player)
+      const sorted = Object.values(this.players).map(p => this.resolve(propertyName, p)).sort((a, b) => (a > b) ? 1 : -1).filter((value, index, self) => self.indexOf(value) === index)
       const index = sorted.indexOf(playerValue)
       const share = index / sorted.length
       const hue = ((reverse ? (1 - share) : share) * 120).toString(10)
@@ -67,7 +78,6 @@ export default {
       return usersData.map((player) => {
         return {
           ...player,
-          alias: player.alias,
           ships_production: player.total_industry * (player.tech.manufacturing.level + 5),
           money_production: (player.total_economy * 10) + (player.tech.banking.level * 75)
         }
